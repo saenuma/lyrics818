b059f0e3e0eea452e7f9b9c94ae8732095f8d811
Bankole Ojo <ojobankole@gmail.com>
2024-08-05 18:22:52 +0100

    Revert "removed windows version"
    This reverts commit 80c049b5e2a1be2b987be28fc5a663f351c71a41.

---
+++ windows/embed.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	_ "embed"
+)
+
+//go:embed Roboto-Light.ttf
+var DefaultFont []byte
+
+//go:embed "bmtf.txt"
+var SampleLyricsFile []byte
+
+//go:embed colors.txt
+var Colors2 []byte
+
+//go:embed "ffmpeg/ffmpeg.exe"
+var ffmpegBytes []byte
