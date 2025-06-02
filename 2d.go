cc3465f833c1d820085807f3f71aa33e9a97a8c0
Bankole Ojo <ojobankole@gmail.com>
2025-06-02 07:43:42 +0100

    refactoring: now using local 2d functions

---
+++ 2d.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"image"
+
+	g143 "github.com/bankole7782/graphics143"
+	"github.com/fogleman/gg"
+)
+
+type Ctx struct {
+	WindowWidth  int
+	WindowHeight int
+	ggCtx        *gg.Context
+	OldFrame     image.Image
+}
+
+func New2dCtx(wWidth, wHeight int) Ctx {
+	// frame buffer
+	ggCtx := gg.NewContext(wWidth, wHeight)
+
+	// background rectangle
+	ggCtx.DrawRectangle(0, 0, float64(wWidth), float64(wHeight))
+	ggCtx.SetHexColor("#ffffff")
+	ggCtx.Fill()
+
+	// load font
+	fontPath := GetDefaultFontPath()
+	err := ggCtx.LoadFontFace(fontPath, 20)
+	if err != nil {
+		panic(err)
+	}
+
+	ctx := Ctx{WindowWidth: wWidth, WindowHeight: wHeight, ggCtx: ggCtx}
+	return ctx
+}
+
+func Continue2dCtx(img image.Image) Ctx {
+	ggCtx := gg.NewContextForImage(img)
+
+	// load font
+	fontPath := GetDefaultFontPath()
+	err := ggCtx.LoadFontFace(fontPath, 20)
+	if err != nil {
+		panic(err)
+	}
+
+	ctx := Ctx{WindowWidth: img.Bounds().Dx(), WindowHeight: img.Bounds().Dy(), ggCtx: ggCtx}
+	return ctx
+}
+
+func (ctx *Ctx) drawButtonA(btnId, originX, originY int, text, textColor, bgColor string) g143.Rect {
+	// draw bounding rect
+	textW, textH := ctx.ggCtx.MeasureString(text)
+	width, height := textW+50, textH+25
+	ctx.ggCtx.SetHexColor(bgColor)
+	ctx.ggCtx.DrawRectangle(float64(originX), float64(originY), float64(width), float64(height))
+	ctx.ggCtx.Fill()
+
+	// draw text
+	ctx.ggCtx.SetHexColor(textColor)
+	ctx.ggCtx.DrawString(text, float64(originX)+25, float64(originY)+FontSize+5)
+
+	// save dimensions
+	btnARect := g143.NewRect(originX, originY, int(width), int(height))
+	ObjCoords[btnId] = btnARect
+	return btnARect
+}
+
+func (ctx *Ctx) drawButtonB(btnId, originX, originY int, text, textColor, bgColor, circleColor string) g143.Rect {
+	// draw bounding rect
+	textW, textH := ctx.ggCtx.MeasureString(text)
+	width, height := textW+80, textH+30
+	ctx.ggCtx.SetHexColor(bgColor)
+	ctx.ggCtx.DrawRectangle(float64(originX), float64(originY), float64(width), float64(height))
+	ctx.ggCtx.Fill()
+
+	// draw text
+	ctx.ggCtx.SetHexColor(textColor)
+	ctx.ggCtx.DrawString(text, float64(originX)+20, float64(originY)+FontSize+10)
+
+	// draw circle
+	ctx.ggCtx.SetHexColor(circleColor)
+	ctx.ggCtx.DrawCircle(float64(originX)+width-30, float64(originY)+(height/2), 10)
+	ctx.ggCtx.Fill()
+
+	// save dimensions
+	btnARect := g143.NewRect(originX, originY, int(width), int(height))
+	ObjCoords[btnId] = btnARect
+	return btnARect
+}
+
+func nextHorizontalX(aRect g143.Rect, margin int) int {
+	return aRect.OriginX + aRect.Width + margin
+}
+
+func nextHorizontalY(aRect g143.Rect, margin int) int {
+	return aRect.OriginY + aRect.Height + margin
+}
--- view_first.go
+++ view_first.go
@@ -21,173 +21,42 @@ func GetDefaultFontPath() string {
 func AllDraws(window *glfw.Window) {
 	wWidth, wHeight := window.GetSize()
 
-	// frame buffer
-	ggCtx := gg.NewContext(wWidth, wHeight)
+	theCtx := New2dCtx(wWidth, wHeight)
 
-	// background rectangle
-	ggCtx.DrawRectangle(0, 0, float64(wWidth), float64(wHeight))
-	ggCtx.SetHexColor("#ffffff")
-	ggCtx.Fill()
-
-	// load font
-	fontPath := GetDefaultFontPath()
-	err := ggCtx.LoadFontFace(fontPath, 20)
-	if err != nil {
-		panic(err)
-	}
-
-	// open working directory button
-	beginXOffset := 80
-	ggCtx.SetHexColor("#D09090")
-	owdStr := "Open Working Directory"
-	owdStrW, owdStrH := ggCtx.MeasureString(owdStr)
-	ggCtx.DrawRectangle(float64(beginXOffset), 10, owdStrW+50, owdStrH+25)
-	ggCtx.Fill()
-
-	owdBtnRS := g143.Rect{Width: int(owdStrW) + 50, Height: int(owdStrH) + 25, OriginX: beginXOffset, OriginY: 10}
-	ObjCoords[OpenWDBtn] = owdBtnRS
-
-	ggCtx.SetHexColor("#444")
-	ggCtx.DrawString(owdStr, float64(beginXOffset)+25, 35)
-
-	// view sample lyrics button
-	ggCtx.SetHexColor("#90D092")
-	vslStr := "View Sample Lyrics"
-	vslStrWidth, vslStrHeight := ggCtx.MeasureString(vslStr)
-	nexBtnOriginX := owdBtnRS.OriginX + owdBtnRS.Width + 30
-	ggCtx.DrawRectangle(float64(nexBtnOriginX), 10, vslStrWidth+50, vslStrHeight+25)
-	ggCtx.Fill()
-
-	vslBtnRS := g143.Rect{Width: int(vslStrWidth) + 50, Height: int(vslStrHeight) + 25, OriginX: nexBtnOriginX,
-		OriginY: 10}
-	ObjCoords[ViewLyricsBtn] = vslBtnRS
-
-	ggCtx.SetHexColor("#444")
-	ggCtx.DrawString(vslStr, float64(vslBtnRS.OriginX)+25, 35)
+	oWDRect := theCtx.drawButtonA(OpenWDBtn, 80, 10, "Open Working Directory", "#444", "#D09090")
+	vSX := nextHorizontalX(oWDRect, 30)
+	theCtx.drawButtonA(ViewLyricsBtn, vSX, 10, "View Sample Lyrics", "#444", "#90D092")
 
 	// Help messages
 	msg1 := "1. All files must be placed in the working directory of this program."
 	msg2 := "2. The background_file must be of dimensions (1366px x 768px)"
 
-	ggCtx.DrawString(msg1, 60, 90+FontSize)
-	ggCtx.DrawString(msg2, 60, 90+30+FontSize)
+	theCtx.ggCtx.DrawString(msg1, 60, 90+FontSize)
+	theCtx.ggCtx.DrawString(msg2, 60, 90+30+FontSize)
 
-	// lyrics file button
-	lfStr := "Select Lyrics File (.txt)"
-	lfStrW, _ := ggCtx.MeasureString(lfStr)
-	ggCtx.SetHexColor("#5F699F")
-	ggCtx.DrawRectangle(40, 160, lfStrW+40, 40)
-	ggCtx.Fill()
+	sLBRect := theCtx.drawButtonA(SelectLyricsBtn, 40, 160, "Select Lyrics File (.txt)", "#fff", "#5F699F")
+	sFBY := nextHorizontalY(sLBRect, 20)
+	sFBRect := theCtx.drawButtonA(FontFileBtn, 40, sFBY, "Select Font File (.ttf)", "#fff", "#5F699F")
+	sBBY := nextHorizontalY(sFBRect, 20)
+	sBBRect := theCtx.drawButtonA(BgFileBtn, 40, sBBY, "Select Background File (.png)", "#fff", "#5F699F")
+	mBBY := nextHorizontalY(sBBRect, 20)
+	mBBRect := theCtx.drawButtonA(MusicFileBtn, 40, mBBY, "Select Music File (.mp3)", "#fff", "#5F699F")
+	lBBY := nextHorizontalY(mBBRect, 20)
+	lBBRect := theCtx.drawButtonA(LyricsColorBtn, 40, lBBY, "Pick Lyrics Color", "#fff", "#5F699F")
 
-	lfrs := g143.NewRect(40, 160, int(lfStrW+40), 40)
-	ObjCoords[SelectLyricsBtn] = lfrs
-
-	ggCtx.SetHexColor("#fff")
-	ggCtx.DrawString(lfStr, 60, 165+FontSize)
-
-	// font file button
-	ffStr := "Select Font File (.ttf)"
-	ffStrW, _ := ggCtx.MeasureString(ffStr)
-	ggCtx.SetHexColor("#5F699F")
-	ggCtx.DrawRectangle(40, 220, ffStrW+40, 40)
-	ggCtx.Fill()
-
-	ffrs := g143.NewRect(40, 220, int(ffStrW+40), 40)
-	ObjCoords[FontFileBtn] = ffrs
-
-	ggCtx.SetHexColor("#fff")
-	ggCtx.DrawString(ffStr, 60, 225+FontSize)
-
-	// background file button
-	bfStr := "Select Background File (.png)"
-	bfStrW, _ := ggCtx.MeasureString(bfStr)
-	ggCtx.SetHexColor("#5F699F")
-	ggCtx.DrawRectangle(40, 280, bfStrW+40, 40)
-	ggCtx.Fill()
-
-	bfrs := g143.NewRect(40, 280, int(bfStrW+40), 40)
-	ObjCoords[BgFileBtn] = bfrs
-
-	ggCtx.SetHexColor("#fff")
-	ggCtx.DrawString(bfStr, 60, 285+FontSize)
-
-	// music file button
-	mfStr := "Select Music File (.mp3)"
-	mfStrW, _ := ggCtx.MeasureString(mfStr)
-	ggCtx.SetHexColor("#5F699F")
-	ggCtx.DrawRectangle(40, 340, mfStrW+40, 40)
-	ggCtx.Fill()
-
-	mfrs := g143.NewRect(40, 340, int(mfStrW+40), 40)
-	ObjCoords[MusicFileBtn] = mfrs
-
-	ggCtx.SetHexColor("#fff")
-	ggCtx.DrawString(mfStr, 60, 345+FontSize)
-
-	// lyrics color button
-	lcStr := "Pick Lyrics Color"
-	lcStrW, _ := ggCtx.MeasureString(lcStr)
-	ggCtx.SetHexColor("#5F699F")
-	ggCtx.DrawRectangle(40, 400, lcStrW+40, 40)
-	ggCtx.Fill()
-
-	lcrs := g143.NewRect(40, 400, int(lcStrW+40), 40)
-	ObjCoords[LyricsColorBtn] = lcrs
-
-	ggCtx.SetHexColor("#fff")
-	ggCtx.DrawString(lcStr, 60, 405+FontSize)
-
-	// render button
-	beginXOffset2 := 220
-	ggCtx.SetHexColor("#A965B5")
-	rStr := "Make Lyrics Video (.mp4)"
-	rStrW, rStrH := ggCtx.MeasureString(rStr)
-	ggCtx.DrawRectangle(float64(beginXOffset2), 500, rStrW+90, rStrH+25)
-	ggCtx.Fill()
-	ggCtx.SetHexColor("#5D435E")
-	ggCtx.DrawRoundedRectangle(float64(beginXOffset2)+rStrW+40, 500+10, 20, 20, 10)
-	ggCtx.Fill()
-
-	rBtnRS := g143.Rect{Width: int(rStrW) + 90, Height: int(rStrH) + 25, OriginX: beginXOffset2, OriginY: 500}
-	ObjCoords[RenderBtn] = rBtnRS
-
-	ggCtx.SetHexColor("#fff")
-	ggCtx.DrawString(rStr, float64(beginXOffset2)+25, 505+FontSize)
-
-	// render l8f button
-	ggCtx.SetHexColor("#674C6A")
-	rl8L := "Make Lyrics Video (.l8f)"
-	rl8LW, rl8LH := ggCtx.MeasureString(rl8L)
-	ggCtx.DrawRectangle(float64(beginXOffset2), 560, rl8LW+70, rl8LH+25)
-	ggCtx.Fill()
-
-	ggCtx.SetHexColor("#fff")
-	ggCtx.DrawRoundedRectangle(float64(beginXOffset2)+rl8LW+40, 560+10, 20, 20, 10)
-	ggCtx.Fill()
-
-	rl8BtnRS := g143.NewRect(beginXOffset2, 560, int(rl8LW)+70, int(rl8LH)+25)
-	ObjCoords[RenderL8fBtn] = rl8BtnRS
-
-	ggCtx.SetHexColor("#fff")
-	ggCtx.DrawString(rl8L, float64(beginXOffset2)+25, 565+FontSize)
-
-	// draw our site below
-	ggCtx.SetHexColor("#9C5858")
-	fromAddr := "sae.ng"
-	fromAddrWidth, fromAddrHeight := ggCtx.MeasureString(fromAddr)
-	fromAddrOriginX := (wWidth - int(fromAddrWidth+20)) / 2
-	ggCtx.DrawString(fromAddr, float64(fromAddrOriginX)+10, float64(wHeight-int(fromAddrHeight)))
-	fars := g143.Rect{OriginX: fromAddrOriginX, OriginY: wHeight - 40,
-		Width: int(fromAddrWidth + 20), Height: 40}
-	ObjCoords[OurSite] = fars
+	rBX := 220
+	rBY := nextHorizontalY(lBBRect, 40)
+	rBBrect := theCtx.drawButtonB(RenderBtn, rBX, rBY, "Make Lyrics Video (.mp4)", "#fff", "#A965B5", "#5D435E")
+	rLBY := nextHorizontalY(rBBrect, 20)
+	theCtx.drawButtonB(RenderL8fBtn, rBX, rLBY, "Make Lyrics Video (.l8f)", "#fff", "#674C6A", "#fff")
 
 	// send the frame to glfw window
 	windowRS := g143.Rect{Width: wWidth, Height: wHeight, OriginX: 0, OriginY: 0}
-	g143.DrawImage(wWidth, wHeight, ggCtx.Image(), windowRS)
+	g143.DrawImage(wWidth, wHeight, theCtx.ggCtx.Image(), windowRS)
 	window.SwapBuffers()
 
 	// save the frame
-	EmptyFrameNoInputs = ggCtx.Image()
+	EmptyFrameNoInputs = theCtx.ggCtx.Image()
 }
 
 func RefreshInputsOnWindow(window *glfw.Window, frame image.Image) image.Image {
